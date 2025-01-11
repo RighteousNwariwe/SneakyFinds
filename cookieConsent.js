a0c4e86d513efaa9099e2cade139c08b73c9ab02
Righteous074 <110613027+Righteous074@users.noreply.github.com>
2025-01-11 03:45:42 +0200

    almost done with the index page

---
+++ cookieConsent.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var cookieConsent = document.getElementById("cookieConsent");
+    var acceptCookies = document.getElementById("acceptCookies");
+  
+    // Check if the user has already accepted cookies
+    if (!getCookie("cookiesAccepted")) {
+      cookieConsent.style.display = "block";
+    }
+  
+    acceptCookies.addEventListener("click", function() {
+      setCookie("cookiesAccepted", "true", 365);
+      cookieConsent.style.display = "none";
+    });
+  
+    function setCookie(name, value, days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+  
+    function getCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    }
+  });
